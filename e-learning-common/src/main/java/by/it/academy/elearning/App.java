91d08a1c086a05bab37c8d3f59a590e05f565e97
Alexander Zenkevich <azenkevich@productiveedge.com>
2019-12-06 15:26:44 +0300

    create infrastructure

---
+++ e-learning-common/src/main/java/by/it/academy/elearning/App.java
@@ -0,0 +1,10 @@
+package by.it.academy.elearning;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
